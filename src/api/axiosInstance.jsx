3fe96959f2df23f7ff1e0e8bdeb7bfea7a83b6dc
Hola <albertopardo301@gmail.com>
2025-10-29 17:12:48 -0700

    true

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:3033/api",
-  withCredentials: false, // Permite el uso de cookies
+  withCredentials: true, // Permite el uso de cookies
   headers: {
     "Content-Type": "application/json",
   },
